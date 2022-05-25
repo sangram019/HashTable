91f02bd0dd20114ce97c8f6ef39b2f5dc99a8653
Sangram <sangram15.behera@gmail.com>
2022-05-26 01:08:55 +0530

    Find Frequency Of Words

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using HashTable;
+namespace HashTable
+{
+
+    class program
+    {
+
+        static void Main(string[] args)
+        {
+            MyMapNode<string, int> hash = new MyMapNode<string, int>(5);
+            string words = "to be or not to be";
+            string[] arr = words.Split(' ');
+            LinkedList<string> checkForDuplication = new LinkedList<string>();
+            foreach (string element in arr) 
+            {
+                int count = 0;
+                foreach (string match in arr)  
+                {
+                    if (element == match)
+                    {
+                        count++;
+                        if (checkForDuplication.Contains(element))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+
+                if (!checkForDuplication.Contains(element))
+                {
+                    checkForDuplication.AddLast(element);
+                    hash.Add(element, count);
+                }
+            }
+            hash.Display();
+        }
+    }
+}
